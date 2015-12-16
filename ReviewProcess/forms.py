b3f7fe8a2b95b6e996937ec0f6e798eeae0ca401
Kiran Patle <tkiran@zeomega.com>
2015-12-16 20:48:20 +0530

    Update Profile Implementation and other changes

--- ReviewProcess/forms.py
+++ ReviewProcess/forms.py
@@ -1,5 +1,13 @@
 from django import forms
+from ReviewProcess.models import UpdateUserInfo
+
 class ContactForm(forms.Form):
     phone = forms.CharField(max_length=100)
     emailid = forms.CharField(max_length=100)
-    
+
+class UpdateProfileForm(forms.ModelForm):
+    """
+    """
+    class Meta:
+        model=UpdateUserInfo
+        exclude=()
--- ReviewProcess/models.py
+++ ReviewProcess/models.py
@@ -8,11 +8,13 @@ class ReportingManagerProfile(models.Model):
 
 class ReviewQuestion(models.Model):
 
-    competencies = models.CharField(
-        max_length=255,
-    )
+    competencies = models.CharField(max_length=255,)
     details = models.CharField(max_length=255,)
-    percentages = models.CharField(
-        max_length=255,
-    )
+    percentages = models.CharField(max_length=255,)
     designation=models.CharField(max_length=255,)
+
+class UpdateUserInfo(models.Model):
+    """
+    """
+    last_name = models.CharField(max_length=50)
+    email_id = models.EmailField()
